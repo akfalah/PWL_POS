59271483919752e2b80473940327ddaba423352d
akfalah <akfalah1107@gmail.com>
2024-03-10 15:27:51 +0700

    Jobsheet04: praktikum 2.1 - Retrieving Single Models & Praktikum 2.2 - Not Found Exceptions

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,7 +11,6 @@ class UserController extends Controller
     public function index()
     {
         // $user = UserModel::all();
-        // return view('user', ['data' => $user]);
 
         // $data = [
         //     'username' => 'customer-1',
@@ -22,7 +21,6 @@ class UserController extends Controller
 
         // UserModel::insert($data);
         // $user = UserModel::all();
-        // return view('user', ['data' => $user]);
 
         // $data = [
         //     'nama' => 'Pelanggan Pertama',
@@ -30,17 +28,34 @@ class UserController extends Controller
 
         // UserModel::where('username', 'customer-1')->update($data);
         // $user = UserModel::all();
-        // return view('user', ['data' => $user]);
 
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345'),
-        ];
-        UserModel::create($data);
+        // $data = [
+        //     'level_id' => 2,
+        //     'username' => 'manager_tiga',
+        //     'nama' => 'Manager 3',
+        //     'password' => Hash::make('12345'),
+        // ];
+        // UserModel::create($data);
 
-        $user = UserModel::all();
+        // $user = UserModel::all();
+
+        // $user = UserModel::find(1);
+
+        // $user = UserModel::where('level_id', 1)->first();
+
+        // $user = UserModel::firstWhere('level_id', 1);
+
+        // $user = UserModel::findOr(5, ['username', 'nama'], function() {
+        //     abort(404);
+        // });
+
+        // $user = UserModel::findOr(20, ['username', 'nama'], function() {
+        //     abort(404);
+        // });
+
+        // $user = UserModel::findOrFail(1);
+        
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
         return view('user', ['data' => $user]);
     }
 }

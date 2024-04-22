0ba6fc9c64eb5cfbce1dd4de1b42a3706c04fd3a
akfalah <akfalah1107@gmail.com>
2024-04-22 14:42:10 +0700

    Jobsheet09 – Authentication, Middleware

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
-class UserModel extends Model
+class UserModel extends User
 {
     use HasFactory;
 

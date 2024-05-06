f491d578d707ab421348ba3a7e8e840434013890
akfalah <akfalah1107@gmail.com>
2024-05-07 00:19:41 +0700

    jobsheet11: Restful API 2

--- app/Http/Controllers/Api/RegisterController.php
+++ app/Http/Controllers/Api/RegisterController.php
@@ -13,10 +13,11 @@ class RegisterController extends Controller
     {
         // set validation
         $validator = Validator::make($request->all(), [
-            'username' => 'required',
-            'nama' => 'required',
-            'password' => 'required|min:5|confirmed',
-            'level_id' => 'required',
+            'username'  => 'required',
+            'nama'      => 'required',
+            'password'  => 'required|min:5|confirmed',
+            'level_id'  => 'required',
+            'image'     => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048'
         ]);
 
         // if validations fails
@@ -24,12 +25,15 @@ class RegisterController extends Controller
             return response()->json($validator->errors(), 422);
         }
 
+        $image = $request->image;
+
         // create user
         $user = UserModel::create([
-            'username' => $request->username,
-            'nama' => $request->nama,
-            'password' => bcrypt($request->password),
-            'level_id' => $request->level_id
+            'username'  => $request->username,
+            'nama'      => $request->nama,
+            'password'  => bcrypt($request->password),
+            'level_id'  => $request->level_id,
+            'image'     => $image->hashName()
         ]);
 
         // return response JSON user is created
